f4123b71bc46d0ffc06155e806d37e452d232db5
Jaime Brolesi <jaime.brolesi@brainweb.com.br>
2020-01-09 11:35:16 -0300

    Initial commit

---
+++ core/src/main/java/br/com/brainweb/interview/core/features/hero/HeroController.java
@@ -0,0 +1,32 @@
+package br.com.brainweb.interview.core.features.hero;
+
+import br.com.brainweb.interview.model.request.CreateHeroRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static java.lang.String.format;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.ResponseEntity.created;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/v1/heroes", produces = APPLICATION_JSON_VALUE)
+public class HeroController {
+
+    private final HeroService heroService;
+
+    @PostMapping(consumes = APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> create(@Validated
+                                       @RequestBody CreateHeroRequest createHeroRequest) {
+        final UUID id = heroService.create(createHeroRequest);
+        return created(URI.create(format("/api/v1/heroes/%s", id))).build();
+    }
+}
---
+++ core/src/main/java/br/com/brainweb/interview/core/features/powerstats/PowerStatsService.java
@@ -0,0 +1,20 @@
+package br.com.brainweb.interview.core.features.powerstats;
+
+import br.com.brainweb.interview.model.PowerStats;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class PowerStatsService {
+
+    private final PowerStatsRepository powerStatsRepository;
+
+    @Transactional
+    public UUID create(PowerStats powerStats) {
+        return powerStatsRepository.create(powerStats);
+    }
+}
