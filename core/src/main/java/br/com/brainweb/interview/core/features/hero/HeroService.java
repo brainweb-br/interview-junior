c575b5d824dfc7f82cfe375801672da2a24a704c
Daniel Brai <danielgm@ciandt.com>
2020-07-19 08:44:14 -0300

    [Task#1] Correção do Bug de Cadastro de Herois
    * O bug era causado pois ao criar um herói não era criado antes um Power
    Stats para o mesmo.
    
    * O pom.xml precisou ser alterado para a execução do projeto,
    explicitando a versão do lombok usada; estudando como fazer para deixar
    esse ponto parametrizado.

--- core/src/main/java/br/com/brainweb/interview/core/features/hero/HeroService.java
+++ core/src/main/java/br/com/brainweb/interview/core/features/hero/HeroService.java
@@ -1,11 +1,14 @@
 package br.com.brainweb.interview.core.features.hero;
 
+import br.com.brainweb.interview.core.features.powerstats.PowerStatsService;
 import br.com.brainweb.interview.model.Hero;
+import br.com.brainweb.interview.model.PowerStats;
 import br.com.brainweb.interview.model.request.CreateHeroRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -14,8 +17,12 @@ public class HeroService {
 
     private final HeroRepository heroRepository;
 
+    private final PowerStatsService powerStatsService;
+
     @Transactional
     public UUID create(CreateHeroRequest createHeroRequest) {
-        return heroRepository.create(new Hero(createHeroRequest, null));
+
+        UUID poweStatsId = this.powerStatsService.create(new PowerStats(createHeroRequest));
+        return heroRepository.create(new Hero(createHeroRequest, poweStatsId));
     }
 }
