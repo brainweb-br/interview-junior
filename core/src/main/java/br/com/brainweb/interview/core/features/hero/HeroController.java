b7107f62431454ae681d6d11bfb2c935a7ab219b
rhz17 <coolerdzn_12@outlook.com>
2021-08-31 23:00:12 -0300

    NullPointerException fix

--- core/src/main/java/br/com/brainweb/interview/core/features/hero/HeroController.java
+++ core/src/main/java/br/com/brainweb/interview/core/features/hero/HeroController.java
@@ -4,10 +4,7 @@ import br.com.brainweb.interview.model.request.CreateHeroRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.UUID;
